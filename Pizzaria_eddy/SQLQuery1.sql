4897183d026eef62d42f90ab9cfa53f19d1efc68
Ismael321 <47277351+Ismael321@users.noreply.github.com>
2019-05-01 15:35:28 -0500

    Add files via upload

---
+++ Pizzaria_eddy/SQLQuery1.sql
@@ -0,0 +1,19 @@
+﻿create database Pizza;
+
+use Pizza;
+
+create table usuarios(
+			id_cliente int not null primary key identity(1,1),
+			nombre varchar(30),
+			contrasena varchar(30),
+			t_usuario varchar(15)
+			)
+
+insert usuarios(nombre , contrasena ,t_usuario )
+values('Ismael' , '2111' ,'administrador' )
+insert usuarios(nombre , contrasena ,t_usuario )
+values('Jose' , '2116' ,'usuario' )
+
+
+select *from usuarios;
+drop table usuarios;
